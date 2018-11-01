0f79c6f9356410ca9b93f000d5d8a1c72e8ffe57
Nicholas Richman <nrichman962@gmail.com>
2018-10-31 22:31:32 -0500

    Added cursor overlay and tile selector
    Added a cursor image to overlay the players image. This script is also
    used to draw a 'selector' icon at the tile coordinates under the mouse.
    This will later be used to showcase what the mouse is selecting and
    allow the player to interact with it accordingly.

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+
+    public Grid mGrid;
+    private Vector3Int mLastPos;
+    private Transform mTileSelector;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = false;
+        mLastPos = new Vector3Int(0, 0, 0);
+        mTileSelector = transform.Find("TileSelector");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = 15;
+        var mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos).toV2();
+        transform.position = mouseWorldPos;
+
+        Vector3Int coordinate = mGrid.WorldToCell(mouseWorldPos);
+        Debug.Log(coordinate);
+        if (mLastPos != coordinate)
+        {
+            mTileSelector.position = coordinate;
+        }
+
+    }
+
+    void drawSelect(Vector3Int coordinate)
+    {
+        Debug.Log("CHANGED");
+    }
+}
---
+++ Assets/Scripts/VectorExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class VectorExtensions {
+    static public Vector3 toV2(this Vector3 vec)
+    {
+        return new Vector3(vec.x, vec.y, 0);
+    }
+}
